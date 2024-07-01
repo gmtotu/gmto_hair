721b9f370ca4d0ccb3da2297819ffd08f8c0ad3f
gmtotu <90359341+gmtotu@users.noreply.github.com>
2024-07-01 16:54:43 -0400

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -3,8 +3,6 @@ ESX = exports["es_extended"]:getSharedObject()
 RegisterNetEvent("makebrobald")
 AddEventHandler("makebrobald", function(source)
     local playerPed = PlayerPedId()
-
-    -- Check if the player has a valid ped
     if DoesEntityExist(playerPed) then
         SetPedComponentVariation(playerPed, 2, 0, 0, 0)
     end
